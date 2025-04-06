17b07e5e458ef69a02356a6dfe77d1434988c837
vinhisreal <vinhquang2610345@gmail.com>
2025-04-06 13:08:22 +0000

    Apr 6, 2025, 8:08 PM

--- client/src/pages/Account/Account.js
+++ client/src/pages/Account/Account.js
@@ -6,17 +6,27 @@ import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import Checkbox from "@mui/material/Checkbox";
 import FormControlLabel from "@mui/material/FormControlLabel";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Cookies from "js-cookie";
 import classNames from "classnames/bind";
 import styles from "./Account.module.scss";
 import Snackbar from "@mui/material/Snackbar";
 import MuiAlert from "@mui/material/Alert";
+import { createAxios } from "../../createAxios";
 
 const cx = classNames.bind(styles);
 
 function Account() {
- return <h1 style={{color:"black", fontSize:"90px"}}>Account</h1> 
+  const currentUser = useSelector((state) => state.user.signin.currentUser);
+  const accessToken = currentUser?.metadata.tokens.accessToken;
+  const userID = currentUser?.metadata.user._id;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const axiosJWT = createAxios(currentUser);
+  const userInfor = currentUser?.metadata.user;
+  console.log(userInfor)
+
+  return <h1 style={{color:"black", fontSize:"90px"}}>Account</h1> 
 }
 
 export default Account;
