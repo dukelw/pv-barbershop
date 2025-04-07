94f2e85d74b8049731f45fad3f5a8839fbd1c579
vinhisreal <vinhquang2610345@gmail.com>
2025-04-07 11:16:04 +0000

    Apr 7, 2025, 6:16 PM

--- client/src/pages/Password/Password.js
+++ client/src/pages/Password/Password.js
@@ -4,7 +4,7 @@ import { createAxios } from "../../createAxios";
 import { changePassword } from "../../redux/apiRequest";
 import { toast } from "react-toastify";
 import classNames from "classnames/bind";
-import styles from "./ChangePassword.module.scss";
+import styles from "./Password.module.scss";
 
 const cx = classNames.bind(styles);
 
