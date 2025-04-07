d3c8a39e232040a432dbe657021f65804f6ead5a
dukelw <thevi16102004@gmail.com>
2025-04-07 22:53:08 +0700

    complete today schedule feature

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -9,7 +9,8 @@ router.post("/other-signup", asyncHandler(userController.signupAnotherWay));
 router.post("/signin", asyncHandler(userController.signin));
 router.post("/signup", asyncHandler(userController.signup));
 router.get("/find/:id", asyncHandler(userController.find));
-router.get("/find-barber", asyncHandler(userController.findAllBarber));
+router.get("/find-barber", asyncHandler(userController.findAllFreeBarber));
+router.get("/barber", asyncHandler(userController.findAllBarber));
 
 router.use(authentication);
 router.post("/logout", asyncHandler(userController.logout));
